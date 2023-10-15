e821bda51720e74b78093a408568ffe420b180a6
Erin Millard <contact@erin.id.au>
2023-10-15 15:12:37 +1000

    Set up repo manager

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ezzatron repositories
---
+++ modules/constants/outputs.tf
@@ -0,0 +1,22 @@
+output "committer" {
+  description = "The committer details to use when committing files to GitHub"
+  value = {
+    name  = "ezzatron-repo-manager[bot]"
+    email = "147964005+ezzatron-repo-manager[bot]@users.noreply.github.com"
+  }
+}
+
+output "license" {
+  description = "The repository license"
+  value       = file("LICENSE")
+}
+
+output "org" {
+  description = "The GitHub organization"
+  value       = "ezzatron"
+}
+
+output "org_name" {
+  description = "The GitHub organization's display name"
+  value       = "ezzatron"
+}
