9b279a5b1d3af993f27554652016af4dc1a71511
Erin Millard <contact@erin.id.au>
2023-10-15 20:48:31 +1000

    Add Renovate repo

--- repos-config.tf
+++ repos-config.tf
@@ -7,3 +7,13 @@ module "repo_dotfiles" {
   has_renovate                 = false
   has_publish_release_workflow = false
 }
+
+module "repo_renovate" {
+  source       = "./modules/repo"
+  name         = "renovate"
+  description  = "Self-hosted Renovate"
+  homepage_url = "https://github.com/ezzatron/renovate/actions/workflows/renovate.yml"
+
+  has_publish_release_workflow = false
+  manage_renovate              = false
+}
