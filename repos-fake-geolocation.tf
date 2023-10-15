983f563a0a69804451e7d0ef67ebca268c9e2ff4
Erin Millard <contact@erin.id.au>
2023-10-15 15:55:48 +1000

    Import nvector-js repo

--- repos-fake-geolocation.tf
+++ repos-fake-geolocation.tf
@@ -10,7 +10,23 @@ module "repo_fake_geolocation_demo" {
   has_release_discussions = true
 }
 
+module "repo_nvector_js" {
+  source      = "./modules/repo"
+  name        = "nvector-js"
+  description = "A suite of tools for solving geographical position calculations"
+
+  ci_workflows = ["node-library"]
+
+  has_discussions         = true
+  has_release_discussions = true
+}
+
 import {
-  to = module.repo_fake_geolocation_demo.github_repository.this
-  id = "fake-geolocation-demo"
+  to = module.repo_nvector_js.github_repository.this
+  id = "nvector-js"
+}
+
+import {
+  to = module.repo_nvector_js.github_branch_protection.default_branch
+  id = "nvector-js:main"
 }
