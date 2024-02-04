53b3ed99b17dfdaf5212f4d73f652661ea34afb1
Erin Millard <contact@erin.id.au>
2024-02-04 16:50:40 +1000

    Remove unused variables

--- variables.tf
+++ variables.tf
@@ -1,16 +1,3 @@
 module "constants" {
   source = "./modules/constants"
 }
-
-variable "GITHUB_APP_ID" {
-  type = string
-}
-
-variable "GITHUB_APP_INSTALLATION_ID" {
-  type = string
-}
-
-variable "GITHUB_APP_PEM_FILE" {
-  type      = string
-  sensitive = true
-}
